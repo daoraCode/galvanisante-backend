16008360c354fc745d656bd09d39849cc823a874
Severin Mb <47704495+daoraCode@users.noreply.github.com>
2023-06-12 16:41:07 +0200

    feat: working on movie routes

--- middlewares/isAuth.js
+++ middlewares/isAuth.js
@@ -4,7 +4,7 @@ export const isAuth = async (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1]; // extract token from header
     req.user = await jwt.verify(token, process.env.TOKEN_SECRET); // verify token
-  console.log(req.user.id);
+    console.log(req.user.id);
     next(); // move to the next function/middleware
   } catch (err) {
     console.log(err);
--- middlewares/isRole.js
+++ middlewares/isRole.js
@@ -1,15 +1,13 @@
-import User from "../models/User.js";
+// import User from "../models/User.js";
 
-// for admin user
+// // for admin user
 
-export const verifyRole = async (req, res, next) => {
-  // main objective is to find email that's match with admin user accordingly
-  const { email } = req.body;
-  const user = await User.findOne({ isAdmin }).contains;
-  if (user) return res.status(200).json({ message: "Not Allowed." });
-  if (user["isAdmin"] === true)
-    return res.status(409).json({ message: "Not Allowed." });
-  next();
-};
-
-
+// export const verifyRole = async (req, res, next) => {
+//   // main objective is to find email that's match with admin user accordingly
+//   const { email } = req.body;
+//   const user = await User.findOne({ isAdmin }).contains;
+//   if (user) return res.status(200).json({ message: "Not Allowed." });
+//   if (user["isAdmin"] === true)
+//     return res.status(409).json({ message: "Not Allowed." });
+//   next();
+// };
